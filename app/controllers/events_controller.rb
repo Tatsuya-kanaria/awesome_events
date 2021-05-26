80c85ab662acaca6f080593b08356eb9af9df414
Tatsuya-kanaria <kanaria.git@gmail.com>
2021-05-26 23:51:44 +0900

    修正

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,7 +9,7 @@ class EventsController < ApplicationController
     @event = current_user.created_events.build(event_params)
 
     if @event.save
-      redirect_to event_path(@event.id), notice: "作成しました"
+      redirect_to @event, notice: "作成しました"
     else
       render :new
     end
