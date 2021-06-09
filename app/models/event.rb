4fbac2e338ace00c0485e2bd19b352dc5d145a5b
Tatsuya-kanaria <kanaria.git@gmail.com>
2021-06-09 21:28:34 +0900

    画像アップロード機能追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
+  before_save :remove_image_if_user_accept
+  attr_accessor :remove_image
+
   with_options presence: true do
     validates :name,    length: { maximum: 50 }
     validates :place,   length: { maximum: 100 }
@@ -7,7 +10,12 @@ class Event < ApplicationRecord
     validates :end_at
   end
   validate :start_at_should_be_before_end_at
+  validates :image,
+    content_type: [:png, :jpg, :jpeg],
+    size: { less_than_or_equal_to: 10.megabytes },
+    dimension: { witdth: { max: 2000 }, height: { max: 2000 } }
 
+  has_one_attached :image, dependent: false
   has_many :tickets, dependent: :destroy
   belongs_to :owner, class_name: 'User'
 
@@ -24,5 +32,9 @@ class Event < ApplicationRecord
       if start_at >= end_at
         errors.add(:start_at, 'は終了時間よりも前に設定してください')
       end
+    end
+
+    def remove_image_if_user_accept
+      self.image = nil if ActiveRecord::Type::Boolean.new.cast(remove_image)
     end
 end
